28dae0064057c093f13388fd2b51eb197eed8cc0
omi265 <omkarmishra22@gmail.com>
2025-02-24 16:24:06 +0530

    Refactor layout in About, Contact, Services, and Team sections for improved spacing and alignment

--- src/components/PageSections/about.tsx
+++ src/components/PageSections/about.tsx
@@ -4,14 +4,14 @@ import React from "react";
 const About = () => {
   return (
     <motion.div
-      className="flex justify-center items-center w-[95%] lg:w-[80%] h-fit lg:h-full pb-[25%] lg:pb-0 pt-[20%] lg:pt-0"
+      className="flex justify-center items-center w-[95%] lg:w-[80%] h-fit lg:h-full pb-[25%] lg:pb-0 pt-[10%] lg:pt-0"
       initial={{ opacity: 0, scale: 0.7 }}
       whileInView={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5, type: "spring" }}
     >
       <div className="flex flex-col justify-start lg:justify-evenly items-center h-full w-full">
-        <div className="elementScroll rounded-2xl w-full lg:h-[70%] space-y-5 lg:space-y-10 lg:overflow-y-scroll lg:snap-y lg:snap-mandatory">
-          <div className=" flex flex-col justify-start lg:justify-center text-3xl lg:text-5xl font-extrabold lg:h-[550px] lg:snap-center text-left lg:mr-5 lg:bg-[#F8C419] lg:border-4 lg:border-black rounded-2xl p-5">
+        <div className="elementScroll rounded-2xl w-full lg:h-fit space-y-5 ">
+          <div className=" flex flex-col justify-start lg:justify-center text-3xl lg:text-5xl font-extrabold lg:h-[550px] lg:snap-center text-left">
             <div>
               <div className="text-center lg:text-left">
                 The Foundation of{" "}
@@ -38,19 +38,6 @@ const About = () => {
               just be seen but be remembered.
             </div>
           </div>
-          {/* <div className=" flex flex-col justify-start lg:justify-center text-3xl lg:text-5xl font-extrabold lg:h-[550px] lg:snap-center text-left lg:mr-5 lg:bg-[#F8C419] lg:border-4 lg:border-black rounded-2xl p-5">
-            <div>
-              <div className="text-center lg:text-left">Core Values</div>
-            </div>
-            <div className="text-lg lg:text-2xl mt-4 font-normal w-full text-justify lg:text-left">
-              While the company name might suggest a bit of Awkwardness, the
-              team embraces the perceived discomfort the name carries, because
-              it shows that one is out of their comfort zone, and that’s where
-              growth and learning take place. Too many brands are too busy
-              trying to be relevant, Awkward Studio, aspires to help brands not
-              just be seen but be remembered.
-            </div>
-          </div> */}
         </div>
       </div>
     </motion.div>
