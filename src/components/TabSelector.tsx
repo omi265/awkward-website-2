30f10eca6d25ed74fae9fdb028df626d84c385e3
omi265 <omkarmishra22@gmail.com>
2025-02-26 12:13:20 +0530

    Adjust width of MobileTabSelector for improved layout consistency on smaller screens

--- src/components/TabSelector.tsx
+++ src/components/TabSelector.tsx
@@ -52,7 +52,7 @@ export const MobileTabSelector = ({
   setSelectedTab,
 }: any) => {
   return (
-    <div className="flex fixed bottom-[5%] justify-center items-center border-r-[0px] border-l-[0px] border-t-[10px] border-black bg-[#D9D9D9] h-12 w-[95.8%] sm:w-[98%] md:w-[99.5%] z-10 text-xs">
+    <div className="flex fixed bottom-[5%] justify-center items-center border-r-[0px] border-l-[0px] border-t-[10px] border-black bg-[#D9D9D9] h-12 w-[95.8%] sm:w-[98.5%] md:w-[99.5%] z-10 text-xs">
       <div className="flex justify-between items-center h-12 divide-x-8 divide-black w-full">
         {tabs.map((tab: any, index: number) => (
           <div
