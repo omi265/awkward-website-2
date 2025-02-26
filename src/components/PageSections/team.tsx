e234e825ff4237fcb03f556363c1b68db2aae86a
omi265 <omkarmishra22@gmail.com>
2025-02-26 13:44:53 +0530

    Update Team component messaging for enhanced clarity and engagement

--- src/components/PageSections/team.tsx
+++ src/components/PageSections/team.tsx
@@ -13,12 +13,14 @@ const Team = () => {
       <div className="flex flex-col justify-start lg:justify-center items-center h-full w-full">
         <div className="flex flex-col justify-center text-3xl lg:text-5xl font-extrabold w-full text-left">
           <div>
-            <div className="text-center lg:text-left">The Team</div>
+            <div className="text-center lg:text-left">
+              Meet the Awkward Geniuses
+            </div>
           </div>
           <div className="text-lg lg:text-2xl mt-4 font-semibold w-full text-justify lg:text-left">
-            Lorem ipsum dolor sit amet consectetur. Turpis dictum mattis
-            suspendisse neque. Auctor et dignissim elementum sed aliquam
-            porttitor diam. Vitae aliquet fames diam curabitur.
+            Great ideas are born from a little chaos, a lot of creativity, and
+            just the right amount of awkward. Our team thrives on turning
+            unconventional thinking into extraordinary solutions.
           </div>
         </div>
         <div className="elementScroll w-full h-[55svh] lg:h-[50svh] overflow-y-scroll snap-y snap-mandatory pr-1 lg:pr-5 mb-[5%] mt-4 lg:mt-8 lg:mb-0">
