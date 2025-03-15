a5eb43c72b6ff8d55f00c5e118818296b654035d
omi265 <omkarmishra22@gmail.com>
2025-03-15 20:20:41 +0530

    Add favicon link to RootLayout for improved branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/ico"
+          sizes="48x48"
+          href="./favicon.ico"
+        ></link>
+      </head>
       <body
         className={`bodyScroll ${geistSans.variable} ${geistMono.variable} antialiased bg-black overscroll-none`}
       >
