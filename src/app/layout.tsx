daae9e087ef34e407f3b4707c8de8342f5238c9b
omi265 <omkarmishra22@gmail.com>
2025-06-04 13:04:47 +0530

    Adding Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import WebWindow from "@/components/WebWindow";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -46,6 +47,27 @@ export default function RootLayout({
         <meta property="og:image" content="./favicon.ico" />
         <meta property="og:url" content="https://www.awkwardstudio.in/" />
         <link rel="canonical" href="https://www.awkwardstudio.in/" />
+
+        {/* Google Tag (gtag.js) scripts */}
+        {/* Script for loading the gtag.js library */}
+        <Script
+          strategy="afterInteractive" // Recommended for analytics to not block rendering
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17028454912"
+        />
+        {/* Inline script for initializing gtag and sending page views */}
+        <Script
+          id="google-analytics" // Unique ID for this script
+          strategy="afterInteractive" // Loads after the page is interactive
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17028454912');
+          `,
+          }}
+        />
+        {/* End Google Tag (gtag.js) scripts */}
       </Head>
       <body
         className={`bodyScroll ${geistSans.variable} ${geistMono.variable} antialiased bg-black overscroll-none`}
