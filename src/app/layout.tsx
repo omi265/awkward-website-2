6a43f6a62fdf398d8c89c6f76440d3760e55a983
omi265 <omkarmishra22@gmail.com>
2025-06-04 14:28:23 +0530

    Adding Google Tag 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,28 +47,27 @@ export default function RootLayout({
         <meta property="og:image" content="./favicon.ico" />
         <meta property="og:url" content="https://www.awkwardstudio.in/" />
         <link rel="canonical" href="https://www.awkwardstudio.in/" />
-
-        {/* Google Tag (gtag.js) scripts */}
-        {/* Script for loading the gtag.js library */}
-        <Script
-          strategy="afterInteractive" // Recommended for analytics to not block rendering
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17028454912"
-        />
-        {/* Inline script for initializing gtag and sending page views */}
-        <Script
-          id="google-analytics" // Unique ID for this script
-          strategy="afterInteractive" // Loads after the page is interactive
-          dangerouslySetInnerHTML={{
-            __html: `
+      </Head>
+      {/* Google Tag (gtag.js) scripts */}
+      {/* Script for loading the gtag.js library */}
+      <Script
+        strategy="afterInteractive" // Recommended for analytics to not block rendering
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17028454912"
+      />
+      {/* Inline script for initializing gtag and sending page views */}
+      <Script
+        id="google-analytics" // Unique ID for this script
+        strategy="afterInteractive" // Loads after the page is interactive
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-17028454912');
           `,
-          }}
-        />
-        {/* End Google Tag (gtag.js) scripts */}
-      </Head>
+        }}
+      />
+      {/* End Google Tag (gtag.js) scripts */}
       <body
         className={`bodyScroll ${geistSans.variable} ${geistMono.variable} antialiased bg-black overscroll-none`}
       >
